d57bf92b94f8ebf01c7022118d86401fe9d1392c
白ちぇす <mons1.zgmf@gmail.com>
2026-09-07 17:39:47 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,5 +2,5 @@
 const APP_VERSION = "1.1.0";
 
 const config = {
-  api: "https://script.google.com/macros/s/AKfycbwCzu3TcpgiDaUjaxFVG3EVpcYLDooRkmWPWAZRWjKbmK1CeZ9uR0eV0FGehrPpmFNY/exec"
+  api: "https://script.google.com/macros/s/AKfycbwBCZansjv3VsaU6UM3lT1dlchpKoakt9F1vZXphbkElvWYZCs51LOrwFk0adNG-HPE/exec"
 };
