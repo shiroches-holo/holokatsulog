e964363d170e33688956692bdfa91404829f88b8
白ちぇす <mons1.zgmf@gmail.com>
2026-09-07 16:34:45 +0900

    Update API endpoint in config.js

--- js/config.js
+++ js/config.js
@@ -2,5 +2,5 @@ config.js// config.js
 const APP_VERSION = "1.1.0";
 
 const config = {
-  api: "https://script.google.com/macros/s/AKfycby0iEcJz769tJVKZOHoqwMCFqyYDal8GeWSeq2l3LGlhhxuAm9Pp0yfnKG2EAWfoT5P/exec"
+  api: "https://script.google.com/macros/s/AKfycbwtSFAu8myGolgFqe23XXUXdAWMMMTOfiV5tBjmZ5f65wr-wtPOE4mNgUr3GG6RkeLx/exec"
 };
