ace6ef27fc654948149b9eb81169f72dc60c5f36
白ちぇす <mons1.zgmf@gmail.com>
2026-09-07 16:21:53 +0900

    Add config.js with application version and API

---
+++ js/config.js
@@ -0,0 +1,6 @@
+config.js// config.js
+const APP_VERSION = "1.1.0";
+
+const config = {
+  api: "https://script.google.com/macros/s/AKfycby0iEcJz769tJVKZOHoqwMCFqyYDal8GeWSeq2l3LGlhhxuAm9Pp0yfnKG2EAWfoT5P/exec"
+};
